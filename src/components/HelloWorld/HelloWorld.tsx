f745495c40519b1143730887facdd32a34445ce0
Leslie Atterbury <atterbur@unlv.nevada.edu>
2025-02-23 21:06:19 -0800

    Changes

---
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const HelloWorld: FC = () => {
+    return (
+        <div
+            className="text-4xl text-cyan-500 bg-black"
+        >
+            Hello World
+        </div>
+    );
+}
+
+export default HelloWorld;
