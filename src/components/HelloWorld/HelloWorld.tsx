472f08fab67726f82baf98343a6b10619a402eef
Leslie Atterbury <atterbur@unlv.nevada.edu>
2025-02-24 12:45:31 -0800

    Update to test component

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const HelloWorld = ( name: string ) => (
-    <div
-        className="text-4xl text-cyan-500 bg-black"
-    >
-        Hello World, {name}
-    </div>
-);
+interface ComponentProps {
+    name: string;
+}
+
+const HelloWorld: React.FC<ComponentProps> = ({ name }) => {
+    return (
+         <div
+            className="text-4xl text-cyan-500 bg-black"
+        >
+            Hello World, {name}
+        </div>
+    );
+};
 
 export default HelloWorld;
